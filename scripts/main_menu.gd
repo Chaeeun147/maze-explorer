41697f7b7cb8e5042bfa535f6539fe4cff28cfe2
Chaeeun147 <chaeeunlim.love@gmail.com>
2026-03-19 20:46:45 -0400

    Level menu
    I added a background and buttons that leads to each level on the level menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+
+func _on_levels_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_menu.tscn")
